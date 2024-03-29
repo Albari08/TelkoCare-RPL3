09e2440dfb465aa40eaa82dd5144cc0ac631e5c3
Albari08 <fatihalbari@student.telkomuniversity.ac.id>
2024-03-29 17:16:44 +0700

    RG3-8 Tampilan untuk jadwal dokter

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
+
+
+Route::get('/dashboard/calendar', function () {
+    return view('/dashboard/calendar');
+});
