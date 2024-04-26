d372620ecbcf34705215c930b4fcb4aed315bb2b
azkanoorhafizhh <azka160103@gmail.com>
2024-04-26 14:47:57 +0700

    Set Up Laravel

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,54 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\AntrianController;
+use App\Http\Controllers\DokterController;
+use App\Http\Controllers\JadwalDokterController;
+use App\Http\Controllers\LogUserController;
+use App\Http\Controllers\UserController;
+use App\Http\Middleware\CekAdmin;
+use App\Http\Middleware\CekPasien;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\CalendarController;
 
+
+
+
+
+
+Route::prefix('pasien')->middleware([CekPasien::class])->group(function () {
+    // Rafly
+    Route::get('antrian', [AntrianController::class, 'index'])->name('pasien.antrian');
+    Route::post('antrian', [AntrianController::class, 'create'])->name('pasien.antrian.create');
+    Route::get('antrian/{id}', [AntrianController::class, 'show'])->name('pasien.antrian.detail');
+    // End Rafly
+});
+Route::prefix('admin')->middleware([CekAdmin::class])->group(function () {
+    // Rafly
+    Route::get('antrian', [AntrianController::class, 'adminIndex'])->name('admin.antrian');
+    Route::get('antrian/{id}', [AntrianController::class, 'adminShow'])->name('admin.antrian.detail');
+    Route::post('antrian/{id}', [AntrianController::class, 'adminStore'])->name('admin-antrian.store');
+    // End Rafly
+    // Azka
+    Route::get('profile', [AdminController::class, 'profileAdmin'])->name('admin-profile-dokter');
+    Route::get('pasien/{id}', [UserController::class, 'adminShow'])->name('admin-profile-pasien-detail');
+    Route::post('pasien/{id}', [UserController::class, 'adminUpdate'])->name('admin.update.pasien');
+    Route::get('jadwal-dokter/{id}',[JadwalDokterController::class, 'adminIndex'])->name('admin.jadwal.dokter');
+    Route::post('jadwal-dokter/{id}',[JadwalDokterController::class, 'adminUpdate'])->name('admin.update.jadwal.dokter');
+    Route::get('detail-aktifitas/{iduser}', [LogUserController::class, 'adminIndex'])->name('admin.log.user');
+
+});
+// Route::get('/rafly/{folder}/{page}', function ($folder, $page) {
+//     return view("rafly.$folder.$page");
+// });
+// Route::get('/{folder}/{page}', function ($folder, $page) {
+//     return view("$folder.$page");
+// });
 Route::get('/', function () {
     return view('welcome');
 });
-
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// });
-
-
-// Route::get('/dashboard/calendar', function () {
-//     return view('/dashboard/calendar');
-// });
-
-Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
-
-Route::get('dashboard/calendar', [CalendarController::class, 'index'])->name('calendar');
+Route::get('/login', [UserController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [UserController::class, 'login']);
+Route::post('/logout', [UserController::class, 'logout'])->name('logout');
+Route::get('/login-admin', [AdminController::class, 'showLoginForm'])->name('login-admin');
+Route::post('/login-admin', [AdminController::class, 'login']);
+Route::post('/logout-admin', [AdminController::class, 'logout'])->name('logout-admin');
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/**/*.{html,js}",
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+        "./node_modules/flowbite/**/*.js",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                Montserrat: ["Montserrat", "sans-serif"],
+            },
+        },
+    },
+    plugins: [require("flowbite/plugin")],
+};
