7bca12bd9462c3fae53b7967bed646781045da5a
sarahpraditya <sarahpraditya@student.telkomuniversity.ac.id>
2024-04-29 13:42:50 +0700

    coba dulu

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
 use App\Http\Controllers\ApotekerController;
 
 Route::get('/', function () {
-    return view('Apoteker/profile');
+    return view('profile');
 });
 
 Route::group(['prefix' => 'apoteker'], function () {
@@ -14,26 +13,3 @@ Route::group(['prefix' => 'apoteker'], function () {
     Route::post('/profile/update-password', 'ApotekerController@updatePassword')->name('apoteker.profile.updatePassword');
     Route::get('/profile/barang-terjual', 'ApotekerController@barangTerjual')->name('apoteker.barangTerjual');
 });
-=======
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('index');
-});
-Route::get('/antrian/{folder}/{page}', function ($folder, $page) {
-    return view("antrian.$folder.$page");
-});
-Route::get('/{folder}/{page}', function ($folder, $page) {
-    return view("$folder.$page");
-});
->>>>>>> dca01c5024df80ffa97d599cd85d9a3bc9320579
