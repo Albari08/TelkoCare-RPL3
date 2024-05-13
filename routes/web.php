5f0db76a2b86ba8ce29d3e7a9501a4417c187265
azkanoorhafizhh <azka160103@gmail.com>
2024-05-13 19:41:24 +0700

    route update

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,16 @@
 
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AntrianController;
+use App\Http\Controllers\BookingController;
 use App\Http\Controllers\DokterController;
 use App\Http\Controllers\JadwalDokterController;
 use App\Http\Controllers\LogUserController;
 use App\Http\Controllers\UserController;
 use App\Http\Middleware\CekAdmin;
 use App\Http\Middleware\CekPasien;
+use App\Http\Middleware\CekDokter;
 use Illuminate\Support\Facades\Route;
+
 
 
 
@@ -21,6 +24,20 @@ Route::prefix('pasien')->middleware([CekPasien::class])->group(function () {
     Route::post('antrian', [AntrianController::class, 'create'])->name('pasien.antrian.create');
     Route::get('antrian/{id}', [AntrianController::class, 'show'])->name('pasien.antrian.detail');
     // End Rafly
+    // Azka
+    Route::get('booking', [DokterController::class, 'booking'])->name('pasien.booking');
+    Route::get('jadwal-dokter/{id}', [JadwalDokterController::class, 'index'])->name('pasien.jadwal.dokter');
+    Route::post('booking/', [BookingController::class, 'store'])->name('pasien.booking.store');
+    Route::get('wait-konfirmasi', [BookingController::class, 'waitKonfirmasi'])->name('pasien.booking.konfirmasi');
+    Route::get('reminder-booking', [BookingController::class, 'reminderBooking'])->name('pasien.reminder.booking');
+    Route::post('ubah-jadwal/{id}', [BookingController::class, 'pasienUbahbooking'])->name('pasien.ubah.booking');
+    // End Azka
+});
+Route::prefix('dokter')->middleware([CekDokter::class])->group(function () {
+    // Azka
+    Route::get('booking', [BookingController::class, 'dokterIndex'])->name('dokter.booking');
+    Route::post('booking/{id}', [BookingController::class, 'dokterUpdate'])->name('dokter.booking.update');
+    // End Azka
 });
 Route::prefix('admin')->middleware([CekAdmin::class])->group(function () {
     // Rafly
@@ -40,9 +57,9 @@ Route::prefix('admin')->middleware([CekAdmin::class])->group(function () {
 // Route::get('/rafly/{folder}/{page}', function ($folder, $page) {
 //     return view("rafly.$folder.$page");
 // });
-Route::get('/{folder}/{page}', function ($folder, $page) {
-    return view("$folder.$page");
-});
+// Route::get('/{folder}/{page}', function ($folder, $page) {
+//     return view("$folder.$page");
+// });
 Route::get('/', function () {
     return view('welcome');
 });
@@ -52,3 +69,6 @@ Route::post('/logout', [UserController::class, 'logout'])->name('logout');
 Route::get('/login-admin', [AdminController::class, 'showLoginForm'])->name('login-admin');
 Route::post('/login-admin', [AdminController::class, 'login']);
 Route::post('/logout-admin', [AdminController::class, 'logout'])->name('logout-admin');
+Route::get('/login-dokter', [DokterController::class, 'showLoginForm'])->name('login-dokter');
+Route::post('/login-dokter', [DokterController::class, 'login']);
+Route::post('/logout-dokter', [DokterController::class, 'logout'])->name('logout-dokter');
