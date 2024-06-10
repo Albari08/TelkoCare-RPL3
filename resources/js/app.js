13ba5ea09a6df70b27ca761a22d1ca2500e2777f
Samajas <samuelpashaginting@student.telkomuniversity.ac.id>
2024-06-10 08:01:13 +0700

    RG3-30 - Dashboard Dokter

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,6 @@
-import './bootstrap';
+require('./bootstrap');
+
+// Import komponen React untuk dashboard
+require('./components/AdminDashboard');
+require('./components/DoctorDashboard');
+require('./components/UserDashboard');
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,13 @@
-import axios from 'axios';
-window.axios = axios;
+window._ = require('lodash');
+
+try {
+    window.Popper = require('popper.js').default;
+    window.$ = window.jQuery = require('jquery');
+
+    require('bootstrap');
+} catch (e) {}
+
+// Meload Axios HTTP library
+window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
